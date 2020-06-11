bc7c32b8777139b5081ff548b044ed0c884e4802
Ani-pejakala <anirudhapejakala@gmail.com>
2020-06-12 00:31:10 +0530

    'initial-commit'

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request,send_file
+import os
+from  run_model import forward_pass
+app = Flask(__name__)
+UPLOAD_FOLDER="/static/uploads/images/"
+STYLE_PATH='static/uploads/style_images/'
+
+@app.route('/',methods=['GET', 'POST'])
+def home_page():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        style=request.form.get('style')
+        if 'file' not in request.files:
+            return render_template('index.html', msg='No file selected')
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return render_template('index.html', msg='No file selected')
+
+        if file and style:
+            print(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
+            file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
+            out=forward_pass(style,file.filename,style)
+            #print(out)
+            filename, file_extension = os.path.splitext(file.filename)
+            return render_template('result.html', image=UPLOAD_FOLDER + file.filename,
+                                   style_image=STYLE_PATH+filename+style+file_extension,style=style)
+        else:
+            return render_template('index.html', msg='No style selected')
+
+    elif request.method == 'GET':
+        return render_template('index.html')
+@app.route('/download/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    print(filename)
+    return send_file(filename,as_attachment=True)
+#@app.route('/upload', methods=['GET', 'POST'])
+#def upload_page():
+
+if __name__ == '__main__':
+    app.run()
---
+++ run_model.py
@@ -0,0 +1,37 @@
+import imutils
+import cv2
+import os
+UPLOAD_PATH='static/uploads/images/'
+MODEL_PATH='static/models/'
+STYLE_PATH='static/uploads/style_images/'
+def forward_pass(model_name,image_name,style):
+    filesToRemove = [os.path.join(STYLE_PATH, f) for f in os.listdir(STYLE_PATH)]
+    for f in filesToRemove:
+        os.remove(f)
+    model=cv2.dnn.readNetFromTorch(MODEL_PATH+model_name)
+    image=cv2.imread(UPLOAD_PATH+image_name)
+    (h,w)=image.shape[:2]
+
+    blob_image=cv2.dnn.blobFromImage(image,1.0,size=(w,h),mean=(103.939, 116.779, 123.680),swapRB=False,crop=False)
+
+    model.setInput(blob_image)
+    output=model.forward()
+
+    output=output.reshape(3,output.shape[2],output.shape[3])
+    output[0]+=103.939
+    output[1]+=116.779
+    output[2]+=123.680
+    #output/=255.0
+    output=output.transpose(1,2,0)
+    #cv2.imshow("out",output)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    #out=cv2.convertScaleAbs(output, alpha=(255.0))
+    filename, file_extension = os.path.splitext(image_name)
+    cv2.imwrite(STYLE_PATH+filename+style+file_extension,output)
+    return output
+
+
+
+
+
